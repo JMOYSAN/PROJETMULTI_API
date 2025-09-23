5d173cec9cd504a4ad3b70af2b674b463847be39
Felty8 <lyambathalon@gmail.com>
2025-09-23 10:06:18 -0400

    fait les routes et le controller

--- api/routes/groups.js
+++ api/routes/groups.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const groupsController = require("../controllers/groupsController");
-const loggingMiddleware = require("../middleware/Logging,js");
+const loggingMiddleware = require("../middleware/Logging.js");
+
 router.use(loggingMiddleware);
 
 router.get("/", groupsController.index);
-router.post("/", groupsController.store);
+
+router.get("/user/:userId", groupsController.userGroupsIndex);
+
+router.get("/public/:userId", groupsController.publicGroupsIndex);
+
+router.get("/next/:type/:lastId", groupsController.nextGroups);
+
+
 router.get("/:id", groupsController.show);
+router.post("/", groupsController.store);
 router.put("/:id", groupsController.update);
 router.delete("/:id", groupsController.destroy);
 
