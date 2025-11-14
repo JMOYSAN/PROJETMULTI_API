82d19b4757ad6e3aa526c3b8d675c58b9322b83b
JMOYSAN <2375142@cstjean.qc.ca>
2025-11-14 13:36:31 -0500

    HOTFIX - Changer le MDP de la BD pour sécuriser le port 3306

--- api/knexfile.js
+++ api/knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
         connection: {
             host: 'mysql',        // service name in docker-compose
             user: 'root',
-            password: 'password',
+            password: '47fc01b0-39ab-4475-a0c6-7b2f6689f2d9',
             database: 'mydb'
         },
         migrations: {
